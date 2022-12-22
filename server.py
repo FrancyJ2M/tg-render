c1629264bdb37f00b2e06f9b97aa3062cbd6958e
FrancyJ2M <97800041+FrancyJ2M@users.noreply.github.com>
2022-12-22 01:25:26 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 from flask import Flask, request
 
-app = Flask(__name__)
+server = Flask(__name__)
 
-@app.route("/")
-def webhook():
-    return "!", 200
-       
-app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 10000)))
+@server.route("/")
+   def webhook():
+       return "!", 200
+   
+server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 10000)))
