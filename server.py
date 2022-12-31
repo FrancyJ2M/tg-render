c81e372265ff5e66914c0c59956081ecf4a40b7a
FrancyJ2M <97800041+FrancyJ2M@users.noreply.github.com>
2022-12-31 17:40:51 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def webhook():
-    return "Solo envie este msg si el bot no responde!\n ✓[MENSAJE DE ACTIVACION]✓", 200
+    return "[MENSAJE DE ACTIVACION]", 200
 
 app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 10000)))
